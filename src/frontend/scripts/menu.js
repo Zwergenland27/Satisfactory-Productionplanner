26bc99817b8692e8531c89e5afb0d59803547d8b
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2023-05-23 23:19:13 +0200

    first menu functions

--- src/frontend/scripts/menu.js
+++ src/frontend/scripts/menu.js
@@ -17,6 +17,7 @@ const menuTemplate = [
         ]
     },
     {
+        label: 'window',
         role: 'windowMenu'
     }
 ]
@@ -24,6 +25,8 @@ const menuTemplate = [
 module.exports = class Menu {
 
     #menuDOM
+    #visible = false
+    #oldDialog
 
     constructor(menuDOM, template = menuTemplate) {
         this.#menuDOM = menuDOM
@@ -35,6 +38,41 @@ module.exports = class Menu {
             let mainItem = document.createElement('span')
             mainItem.textContent = mainMenu.label
             this.#menuDOM.appendChild(mainItem)
+
+            let submenuBox = document.createElement('dialog')
+            submenuBox.classList.add('hidden')
+            this.#menuDOM.appendChild(submenuBox)
+
+            mainItem.addEventListener('click', () => {
+                if(this.#visible) {
+                   this.#visible = false
+                   submenuBox.classList.add('hidden')
+                }
+                else{
+                    this.#visible = true
+                    submenuBox.classList.remove('hidden')
+                }
+            });
+
+            mainItem.addEventListener('mouseenter', () => {
+                if(!this.#visible) return
+                if(this.#oldDialog != null){
+                    this.#oldDialog.classList.add('hidden')
+                }
+                this.#oldDialog = submenuBox
+                submenuBox.classList.remove('hidden')
+            })
+
+            this.#createSubMenu(mainMenu.submenu, submenuBox)
+        })
+    }
+
+    #createSubMenu(menuItems, submenuBox) {
+        if(menuItems == null) return;
+        menuItems.forEach(menu => {
+            let submenuItem = document.createElement('span')
+            submenuItem.textContent = menu.role
+            submenuBox.appendChild(submenuItem)
         })
     }
     addHandler(menuItem, handlerFunction) {
